5054294972700eec018666a46c4c7a6c299462fe
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2023-08-15 23:27:18 -0400

    inital js scripts

---
+++ script.js
@@ -0,0 +1,8 @@
+function addToList() {
+    var item = document.getElementById("item");
+    var date = document.getElementById("date");
+    var amount = document.getElementById("amount");
+    var list = document.getElementById("list");
+
+    
+}
